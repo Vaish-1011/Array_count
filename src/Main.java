df11fbf10eafeff50d683b177369d2af0b9fea84
vaishnavi <vaishumurugaiah@gmail.com>
2023-08-24 19:38:13 +0530

    initial commit done

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] A = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+        }
+        int result = count(A);
+        System.out.println("Number of elements with at least 1 element greater than itself: " + result);
+    }
+    public static int count(int[] A) {
+        int count = 0;
+        int max = Integer.MIN_VALUE;
+        for (int j : A) {
+            max = Math.max(max, j);
+        }
+        for (int j : A) {
+            if (j < max) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
